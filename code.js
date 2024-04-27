29b1f89440a3a4e060bf91e057cf4fe85341d734
BankuBa <paaekow04@gmail.com>
2024-04-27 22:17:02 +0000

    added more colors

--- code.js
+++ code.js
@@ -3,13 +3,21 @@ let index = 0;
 function changeColors() {
   const colors = [
     `red`,
+    `linear-gradient(45deg, red, teal)`,
     `yellow`,
+    `linear-gradient(45deg, yellow, indigo)`,
     `green`,
+    `linear-gradient(55deg, blue, yellow)`,
     `blue`,
+    `linear-gradient(45deg, orange,violet )`,
     `purple`,
+    `linear-gradient(65deg, green, pink)`,
     `orange`,
+    `linear-gradient(45deg, blue, red)`,
     `indigo`,
+    `linear-gradient(45deg, blue, tomayp)`,
     `violet`,
+    `linear-gradient(45deg, orange, white)`,
     `tomato`,
   ];
   document.getElementsByTagName(`body`)[0].style.background = colors[index++]
